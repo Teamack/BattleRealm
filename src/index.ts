a48708852ca3caf8f0647c7db3ade31888b0d3f5
Thomas McTigue <Tom@fuxmart.com>
2025-08-19 06:36:29 -0600

    feat: export card lists from index

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { advancedCreatures } from './advanced-creatures';
+export { basicSpells } from './basic-spells';
+export { advancedSpells } from './advanced-spells';
+export { godTierSpells } from './god-tier-spells';
+export { basicEquipment } from './basic-equipment';
+export { advancedEquipment } from './advanced-equipment';
+export { godTierEquipment } from './god-tier-equipment';
